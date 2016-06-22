e42a6148cf420e01ea4b0d6dd346f65be1dd4ef2
fujimoto <basssaudaji@yahoo.co.jp>
2016-06-22 21:04:35 +0900

    投稿画像の一覧表示

--- Album/show_photos.php
+++ Album/show_photos.php
@@ -1,8 +1,17 @@
 <html>
 <head>
-<title>Album</title>
+    <title>Album</title>
+    <meta charset="utf-8">
 </head>
-
-
-
+    <pre>
+    <?php
+    $dir_file = scandir("../image/");
+    //var_dump($dir_file);
+    foreach($dir_file as $key){
+    ?>
+    <img src="../image/<?=$key?>" alt="" width="200px">
+    <?php
+    }
+    ?>
+    </pre>
 </html>
